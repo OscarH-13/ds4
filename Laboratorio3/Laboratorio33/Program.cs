29a9e1584e8d0b6efae1b48bae473289a77fd3ba
Oscar Herrera <oscar13.herrera@outlook.com>
2025-09-21 02:25:17 -0500

    Laboratorio 3 commit

---
+++ Laboratorio3/Laboratorio33/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int a, b;
+        CalculosMatematicos cm = new CalculosMatematicos();
+        Console.WriteLine("Ingrese el largo del rectangulo ");
+        a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ingrese el ancho del rectangulo ");
+        b = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("El perimetro del triangulo es " + cm.calcularPerimetro(a,b));
+    }
+}
+internal class CalculosMatematicos
+{
+    public int calcularPerimetro(int a, int b)
+    {
+        return 2*(a+b);
+    }
+}
