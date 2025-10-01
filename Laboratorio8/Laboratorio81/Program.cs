3193df01227f675bfbef6964382deef08469a100
Oscar Herrera <oscar13.herrera@outlook.com>
2025-10-01 10:28:54 -0500

    Commit lab8

---
+++ Laboratorio8/Laboratorio81/Program.cs
@@ -0,0 +1,36 @@
+﻿class Persona
+{
+    public string Nombre;
+    public int Edad;
+    public string NIF;
+    void Cumpleaños()
+    {
+        Edad++;
+    }
+    public Persona(string nombre, int edad, string nif)
+    {
+        Nombre = nombre;
+        Edad = edad;
+        NIF = nif;
+    }
+}
+class Trabajador : Persona 
+{
+    public int Sueldo;
+    public Trabajador(string nombre, int edad, string nif, int sueldo) : base(nombre, edad, nif) 
+    {
+        Sueldo = sueldo;
+    }
+}
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Trabajador p = new Trabajador("Josan", 22, "77588260-Z", 100000);
+        Console.WriteLine("Nombre=" + p.Nombre);
+        Console.WriteLine("Edad=" + p.Edad);
+        Console.WriteLine("NIF=" + p.NIF);
+        Console.WriteLine("Sueldo=" + p.Sueldo);
+        Console.ReadKey();
+    }
+}
---
+++ Laboratorio8/Laboratorio86/Program.cs
@@ -0,0 +1,22 @@
+﻿class ClaseBase
+{
+    public void test()
+    { 
+    }
+    public virtual void masTests()
+    { 
+    }
+}
+class ClaseHijo : ClaseBase
+{
+    public override void masTests()
+    { 
+    }
+}
+internal class Program
+{
+    private static void Main(String[] args)
+    {
+        Console.WriteLine("Corrio la aplicacion");
+    }
+}
